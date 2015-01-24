2952e5ca56785556de8fc7d978a0e6f3b2fd772c
Jose Luis Navarro <jlnavarro111@gmail.com>
2015-01-24 17:33:41 +0100

    Dummy agent

---
+++ agent/agent.go
@@ -0,0 +1,42 @@
+package agent
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/alicebob/procspy"
+	"time"
+)
+
+func listener(c <-chan []byte) {
+	for {
+		msg := <-c
+		fmt.Println(string(msg))
+		//time.Sleep(time.Second * 2)
+	}
+}
+
+func startMonitor(channel chan<- []byte, scanningSeconds int64) {
+	for {
+		cs, err := procspy.Connections(true)
+		if err != nil {
+			panic(err)
+		}
+		js, err := json.Marshal(cs)
+		if err != nil {
+			fmt.Println(err)
+		}
+		channel <- js
+		time.Sleep(time.Second * time.Duration(scanningSeconds))
+	}
+
+}
+
+func Run() {
+	fmt.Println("Start agent")
+
+	var c chan []byte = make(chan []byte)
+
+	go printer(c)
+
+	startMonitor(c, 1)
+}
